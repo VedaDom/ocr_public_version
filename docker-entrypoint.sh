a8d8c0d2d946ff1a09547d24baea95747114ae47
Nsengimana Veda Dominique <nsengimanavedadom@gmail.com>
2025-11-12 22:29:17 +0200

    remove db wait

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,28 +3,6 @@ set -euo pipefail
 
 export PYTHONPATH="/app:${PYTHONPATH:-}"
 
-# Wait for the database to be ready if DATABASE_URL is set
-python - <<'PY'
-import os, time, sys
-from sqlalchemy import create_engine, text
-url = os.environ.get('DATABASE_URL') or os.environ.get('database_url') or os.environ.get('DATABASE_URI')
-if not url:
-    print('No database URL set, skipping DB wait')
-    sys.exit(0)
-engine = create_engine(url, pool_pre_ping=True)
-for i in range(60):
-    try:
-        with engine.connect() as conn:
-            conn.execute(text('SELECT 1'))
-        print('Database is ready')
-        sys.exit(0)
-    except Exception as e:
-        print(f'Waiting for database... ({i+1}/60)')
-        time.sleep(1)
-print('Database is not ready after waiting 60s', file=sys.stderr)
-sys.exit(1)
-PY
-
 # Run migrations
 alembic upgrade head
 
